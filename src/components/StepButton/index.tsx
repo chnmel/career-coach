f936c710c441c766c8ed5bc66e3e6bcf9d537142
David Hornsby <dv.opensource@hornsby.at>
2022-05-09 11:57:02 +0200

    Refactoring css to rem instead of px; Added assests folder and initial StepButton icon images; Added iconSrc props to StepButton component;

--- src/components/StepButton/index.tsx
+++ src/components/StepButton/index.tsx
@@ -6,33 +6,35 @@ type StepButtonProps = {
     size: number,
     title: string,
     buttonType: 'inactive' | 'active' | 'done',
+    iconSrcActive: string,
+    iconSrcInactive: string,
 }
 
 const StepButton = (props:StepButtonProps) => {
     const size = props.size;
     const circleWidth = size * 0.1;
     const buttonType = props.buttonType;
-    const ringColor = buttonType == 'active' ? colors.orange : colors.lightgrey;
-    const buttonColor = buttonType == 'inactive' ? colors.lightgrey : buttonType == 'done' ? colors.orange : colors.green;
-    const iconSrc = buttonType == 'active' ? 'https://cdn-icons-png.flaticon.com/512/942/942748.png' : 'https://cdn-icons-png.flaticon.com/512/942/942799.png';
-    const iconOpacity = buttonType == 'active' ? 1 : .5;
+    const ringColor = buttonType === 'active' ? colors.yellow : colors.lightgrey;
+    const buttonColor = buttonType === 'inactive' ? colors.lightgrey : buttonType === 'done' ? colors.yellow : colors.green;
+    const iconSrc = buttonType === 'active' ? props.iconSrcActive : props.iconSrcInactive;
+    const iconOpacity = buttonType === 'active' ? 1 : .5;
     
     return (
-        <div className={styles.container} style={{maxWidth: 2*size}}>
+        <div className={styles.container} style={{maxWidth: 2*size + 'px'}}>
             <div className={styles.outerRing} style={
                 {
-                    height: size, 
-                    width: size,
-                    borderWidth: circleWidth,
+                    height: size + 'px',
+                    width: size + 'px',
+                    borderWidth: circleWidth + 'px',
                     borderColor: ringColor,
-                    padding: circleWidth/2,
+                    padding: circleWidth/2 + 'px',
                 }
             }>
                 <div className={styles.center} style={{backgroundColor: buttonColor}}>
                     <img className={styles.icon} src={iconSrc} alt={props.title} style={{opacity: iconOpacity}}/>
                 </div>
             </div>
-                <h3 className={styles.title} style={{fontSize: size*0.2}}>{props.title}</h3>
+                <h3 className={styles.title} style={{fontSize: size*0.2 + 'px'}}>{props.title}</h3>
         </div>
     );
 };
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,7 +1,7 @@
 const colors = {
-    background: '#F7F0F0',
+    white: '#F7F0F0',
     green: '#66BB63',
-    orange: '#F2BB05',
+    yellow: '#F2BB05',
     darkgrey: '#484349',
     lightgrey: '#C4C4C4',
 }
