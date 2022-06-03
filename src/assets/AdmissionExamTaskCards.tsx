365a86e11c532ad8a30573e2c53be90c619a72c4
David Hornsby <dv.opensource@hornsby.at>
2022-06-03 14:53:02 +0200

    Finished first draft of AdmissionExam screen;

--- src/assets/AdmissionExamTaskCards.tsx
+++ src/assets/AdmissionExamTaskCards.tsx
@@ -10,22 +10,60 @@ const AdmissionExamTaskCards = [
             dein <b>Allgemeinwissen</b> oder bestimmte <b>Fähigkeiten</b> abfragen.</div>,
           <div>Die nächsten Aufgaben zeigen dir was du machen kannst, um für deinen nächsten Eignungstest bereit zu sein.</div>,
           <div style={{textAlign: "center"}}><b>Viel Spaß!</b></div>
-      ,]
+      ]
     },
     {
       id: 1,
       title: 'Aufgaben üben',
-      content: ['content'],
+      content: [
+        <div>Mit etwas <b>Übung</b> kannst du deine Leistung beim Test ganz einfach verbessern.</div>,
+        <div>Damit erhöhst du auch deine Chancen zu einem Bewerbungsgespräch eingeladen zu werden.</div>,
+        <div>Lerne die Antworten in den Übungstests aber <b>nicht auswendig</b>, das wird dir für den echten Test nichts bringen.</div>,
+        <div>Konzentriere dich lieber darauf <b>zu verstehen, warum</b> eine Antwort richtig oder falsch ist.</div>,
+        <div>So beantwortest du ähnliche Fragen richtig, wenns dann darauf ankommt.<br/><br/></div>,
+        <div>Wir haben für dich einige Webseiten gesammelt, wo du <b>gratis Übungstests</b> oder Beispielfragen findest.</div>,
+        <div>Du kannst auch im Internet nach 'Gehirntraining' suchen, da findest du viele lustige Aufgaben, die dir für den Test helfen können.</div>,
+        <a href='https://www.jobbs.at/6-etappe/aufnahmetests/'>jobbs.at</a>,
+        <a href='https://www.einstellungstest-fragen.de/#gratis-training'>einstellungstest-fragen.de</a>,
+        <a href='https://www.ausbildungspark.com/einstellungstest/'>ausbildungspark.com</a>,
+        <a href='https://www.jobbs.at/6-etappe/aufnahmetests/'>karrierebibel.de</a>,
+      ]
     },
     {
       id: 2,
       title: 'Konzentration üben',
-      content: ['content'],
+      content: [
+        <div>Auch wenn du dich mit den Übungstests schon wohl fühlst, gibt es noch ein paar Dinge auf die du aufpassen solltest:</div>,
+        <div>Manchmal haben die Tests so <b>viele Aufgaben</b>, dass du sie gar nicht alle in der vorgegebenen Zeit schaffen kannst.</div>,
+        <div>Das machen Prüfer absichtlich, damit sie sehen wie du <b>mit Stress umgehst</b>.</div>,
+        <div>Lass dich davon also <b>nicht aus der Ruhe bringen</b> und erledige so viele Aufgaben wie möglich.</div>,
+        <div>Wenn dir eine Aufgabe <b>zu schwierig</b> vorkommt, gehe einfach <b>zur nächsten</b> 
+          und heb sie dir für den Schluss auf, falls du noch Zeit hast.<br/><br/></div>,
+        <div>Übe die Tests nochmal aber stelle dir einen <b>Wecker auf 30 Minuten</b>. Probiere in dieser Zeit <b>so viele Fragen wie möglich</b> zu beantworten.
+          Schreib dir danach auf wie viele du <b>geschafft</b> hast und welche du <b>übersprungen</b> hast. Wiederhole das ganze jeden Tag. 
+          Du wirst staunen, wie schnell du dich verbesserst.
+        </div>,
+        <a href='https://www.jobbs.at/6-etappe/aufnahmetests/'>jobbs.at</a>,
+        <a href='https://www.einstellungstest-fragen.de/#gratis-training'>einstellungstest-fragen.de</a>,
+        <a href='https://www.ausbildungspark.com/einstellungstest/'>ausbildungspark.com</a>,
+      ]
     },
     {
       id: 3,
       title: 'Beim Test',
-      content: ['content'],
+      content: [
+        <div>Ein paar kleine Tipps wollen wir dir noch auf den Weg geben.</div>,
+        <div>Informiere dich rechtzeitig <b>wo</b> der Test sein wird (und wie du dort hin kommst), 
+          <b> wann</b> du dort sein solltest (komm lieber ein paar Minuten früher)
+          und <b>was</b> du mitnehmen musst.</div>,
+        <div>Du kannst dir auch schon vorher überlegen, was du anziehst, um möglichst wenig Stress am Tag des Tests zu haben.<br/><br/></div>,
+        <div>Wenn du den Test nicht alleine schreibst, konzentriere dich ganz auf <b>deinen Test</b> und ignoriere was die Anderen machen.</div>,
+        <div>Weiter unten findest du noch Webseiten mit mehr Informationen zu Einstellungstest.</div>,
+        <div style={{textAlign: "center"}}><b>Viel Spaß und Erfolg!</b></div>,
+        <a href='https://www.jobbs.at/6-etappe/aufnahmetests/'>jobbs.at</a>,
+        <a href='https://www.einstellungstest-fragen.de/einstellungstest-die-testsituation/'>einstellungstest-fragen.de</a>,
+        <a href='https://karrierebibel.de/auswahltests/'>karrierebibel.de</a>,
+      ]
     },
   ]
 
